a790b3a69332a15933c5bad7411c42e6955f77f7
Pater-Forg <99220738+Pater-Forg@users.noreply.github.com>
2022-02-08 01:15:56 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,11 @@
+﻿class Person
+{
+    public const string TYPE = "Person";
+    public string Name = "Undefined";
+    public int Age = 0;
+
+    public void Print()
+    {
+        Console.WriteLine($"Type: {TYPE}\nName: {Name}\nAge: {Age}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.Name = "Tom";
+        person.Age = 32;
+        person.Print();
+    }
+}
